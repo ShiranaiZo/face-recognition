d668450bf8db7a7ffd7975e49fe0dacf114c88c5
vnaqilla <tp9518021@gmail.com>
2022-12-26 14:05:00 +0700

    Menambahkan softdelete pada data barang

--- app/Model/Databarang.php
+++ app/Model/Databarang.php
@@ -3,9 +3,11 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Databarang extends Model
 {
+    use SoftDeletes;
     protected $guarded = [
         'id', 'created_at'
     ];
